a12bac877a7a402dc60c106082140ac2e4948b9f
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2023-04-25 15:26:37 +0200

    Plooots

--- ComparisonPlots.py
+++ ComparisonPlots.py
@@ -18,25 +18,39 @@ datafolder = os.path.relpath(r'Data')
 
 # Download precipitation data
 prec = pd.read_excel(os.path.join(datafolder,'CRU_PRE_CPY_1991_2020.xlsx'))# EXCEL File with rainfall time series per subcatchment in mm/month
+pet = pd.read_excel(os.path.join(datafolder,'CRU_PET_CPY_1991_2020.xlsx'), sheet_name = 1)
+grun = pd.read_excel(os.path.join(datafolder, 'G-RUN_CPY_1990_2019.xlsx'), sheet_name=1)
+
 prec_filtered = prec[prec['Month'].isin(ntimestamps)]
 prec_average = prec_filtered.mean(axis = 1)
+
+
+pet_filtered = pet[pet['Month'].isin(ntimestamps)]
+pet_average = pet_filtered.mean(axis = 1)
+
+
+grun_filtered = grun[grun['Month'].isin(ntimestamps)]
+grun_average = grun_filtered.mean(axis = 1)
+
 
 ### Remaking ntimes to a list of all the month names
 month_names = [date.strftime("%b") for date in ntimestamps]
 
 # Plot data and set x-tick labels to January of each year
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, total_release1, label='1')
 plt.plot(ntimes, total_release2, label='2')
 plt.plot(ntimes, total_release3, label='3')
-plt.xticks(ntimes, month_names, rotation=45)
+plt.xticks(ntimes, month_names, rotation=55)
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.legend()
-plt.xlim(0, 12)
+plt.xlim(0, 48)
+plt.ylabel('Total release [MCM]')
 plt.savefig('release_plot.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
 ### Plot precipitation (average over all reservoirs), and total reservoir spill for 3 scenarios
-fig, ax1 = plt.subplots()
+fig, ax1 = plt.subplots(figsize =(15,5))
 # plot the three graphs on the first y-axis
 ax1.plot(ntimes, total_Spill1, label = 'Baseline')
 ax1.plot(ntimes, total_Spill2, label = 'with KST')
@@ -55,11 +69,11 @@ plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12*2, 10+12*2, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12*3, 10+12*3, facecolor='blue', alpha=0.2)
-ax1.legend(loc='upper left', bbox_to_anchor=(0.0, 0.92))
+ax1.legend(loc='upper left', bbox_to_anchor=(0.0, 0.9))
 ax2.legend(loc='upper left', bbox_to_anchor=(0.0, 1.0))
 ax1.set_xticks(ntimes)
-ax1.set_xticklabels(month_names, rotation=45, fontsize  = 8)
-plt.xlim(0,36)
+ax1.set_xticklabels(month_names, rotation=45, fontsize  = 10)
+plt.xlim(0,48)
 plt.savefig('spill_plot.png', bbox_inches='tight',pad_inches = 0.1, dpi=300)
 plt.subplots_adjust(bottom=0.15)
 plt.show()
@@ -74,28 +88,33 @@ deficit_sum1 = (optDInd1.mean(axis=1) + optDAg1.mean(axis = 1) + optDDom1.mean(a
 deficit_sum2 = (optDInd2.mean(axis=1) + optDAg2.mean(axis = 1) + optDDom2.mean(axis = 1))/3
 deficit_sum3 = (optDInd3.mean(axis=1) + optDAg3.mean(axis = 1) + optDDom3.mean(axis = 1))/3
 
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, deficit_sum1, label = 'Baseline')
 plt.plot(ntimes, deficit_sum2, label = 'with KST')
 plt.plot(ntimes, deficit_sum3, label = 'with KST and FS')
-plt.xticks(ntimes, month_names, rotation=45, fontsize  = 8)
+plt.xticks(ntimes, month_names, rotation=45, fontsize  = 10)
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
 plt.legend()
-plt.xlim(0,36)
+plt.ylabel('Mean deficit [MCM]')
+plt.xlim(0,48)
 plt.savefig('deficit_plot.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, optOF1.mean(axis = 1), label = 'Baseline')
 plt.plot(ntimes, optOF2.mean(axis = 1), label = 'with KST')
 plt.plot(ntimes, optOF3.mean(axis = 1), label = 'with KST and FS')
-plt.xticks(ntimes, month_names, rotation=45, fontsize  = 8)
+plt.xticks(ntimes, month_names, rotation=45, fontsize  = 10)
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
-#plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
-#plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12+12+12, 10+12+12+12, facecolor='blue', alpha=0.2)
 plt.legend()
-plt.xlim(0,12)
-plt.savefig('runoff_plot.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
+plt.ylabel('Outflow [MCM]')
+plt.xlim(0,48)
+plt.savefig('Outflow_plot.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
 
@@ -108,26 +127,20 @@ scenario1 = np.array([value(ag_ben1), value(ind_ben1), value(dom_ben1), value(po
 scenario2 = np.array([value(ag_ben2), value(ind_ben2), value(dom_ben2), value(pow_ben2)])
 scenario3 = np.array([value(ag_ben3), value(ind_ben3), value(dom_ben3), value(pow_ben3)])
 
-# set the width of the bars
-barWidth = 0.25
-
-# set the positions of the bars on the x-axis
+plt.figure(figsize=(15,5))
+barWidth = 0.15
 r1 = np.arange(len(scenario1))
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
-
-# create the bar plot
 plt.bar(r1, scenario1, width=barWidth, edgecolor='white', label='Baseline')
 plt.bar(r2, scenario2, width=barWidth, edgecolor='white', label='with KST')
 plt.bar(r3, scenario3, width=barWidth, edgecolor='white', label='with KST +FS')
 
-# add xticks on the middle of the group bars
 plt.xlabel('Benefit')
-plt.xticks([r + barWidth for r in range(len(scenario1))], ['Agriculture', 'Industry', 'Domestric', 'Power'])
+plt.xticks([r + barWidth for r in range(len(scenario1))], ['Agriculture', 'Industry', 'Domestic', 'Power'])
 # add ylabel and title
 plt.ylabel('billion THB per year')
 #plt.title('Values for different variables in three different scenarios')
-
 plt.legend()
 plt.savefig('benefit_barplot.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
@@ -142,16 +155,18 @@ optAAg1 = optAAg1.mean(axis = 1)
 optAAg2 = optAAg2.mean(axis = 1)
 optAAg3 = optAAg3.mean(axis = 1)
 
-
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, optAAg1, label = 'Baseline')
 plt.plot(ntimes, optAAg2, label = 'with KST')
 plt.plot(ntimes, optAAg3, label = 'with KST and FS')
-plt.xticks(ntimes, month_names, rotation=45, fontsize  = 8)
+plt.xticks(ntimes, month_names, rotation=45, fontsize  = 10)
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12+12+12, 10+12+12+12, facecolor='blue', alpha=0.2)
 plt.legend(loc = 'upper right')
-plt.xlim(0,36)
+plt.ylabel('Mean agriculture allocation [MCM]')
+plt.xlim(0,48)
 plt.savefig('Agriculture_Allocation.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
@@ -160,42 +175,74 @@ from SC1_WRS_model import AVpow_gen as AVpow_gen1, SUMpow_gen as SUMpow_gen1
 from SC2_WRS_model import AVpow_gen as AVpow_gen2, SUMpow_gen as SUMpow_gen2
 from SC3_WRS_model import AVpow_gen as AVpow_gen3, SUMpow_gen as SUMpow_gen3
 
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, SUMpow_gen1, label = 'Baseline')
 plt.plot(ntimes, SUMpow_gen2, label = 'with KST')
 plt.plot(ntimes, SUMpow_gen3, label = 'with KST and FS')
-plt.xticks(ntimes, month_names, rotation=45, fontsize  = 8)
+plt.xticks(ntimes, month_names, rotation=45, fontsize  = 10)
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12+12+12, 10+12+12+12, facecolor='blue', alpha=0.2)
 plt.legend(loc='upper right')
-plt.xlim(0,36)
+plt.ylabel('Total power generation [kWh]')
+plt.xlim(0,48)
 plt.savefig('Power_Generation_sum.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
+plt.figure(figsize=(15,5))
 plt.plot(ntimes, AVpow_gen1, label = 'Baseline')
 plt.plot(ntimes, AVpow_gen2, label = 'with KST')
 plt.plot(ntimes, AVpow_gen3, label = 'with KST and FS')
-plt.xticks(ntimes, month_names, rotation=45, fontsize  = 8)
+plt.xticks(ntimes, month_names, rotation=45, fontsize  = 10)
+
 plt.axvspan(7, 10, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12, 10+12, facecolor='blue', alpha=0.2)
 plt.axvspan(7+12+12, 10+12+12, facecolor='blue', alpha=0.2)
+plt.axvspan(7+12+12+12, 10+12+12+12, facecolor='blue', alpha=0.2)
+
 plt.legend(loc='upper right')
-plt.xlim(0,36)
+plt.ylabel('Mean power generation [kWh]')
+plt.xlim(0,48)
 plt.savefig('Power_Generation_AV.png', bbox_inches='tight',pad_inches = 0.1,dpi=300)
 plt.show()
 
 
 
-
-fig, axes = plt.subplots(nrows=len(prec_filtered.columns), ncols=1, figsize = (8,20), constrained_layout=True)
+# Precipitation for all catchments with monsoon
+fig, axes = plt.subplots(nrows=13, ncols=1, figsize = (10,25), constrained_layout=True)
 for i, key in enumerate(prec_filtered.columns):
-    ax = axes[i]
+    if key == 'Month':
+        continue
+    ax = axes[i-1]
     ax.plot(prec_filtered[key], label = key)
     ax.legend(loc = 'upper right', fontsize = 10)
+    for j in prec_filtered.index:
+        if j in monsoon and j-1 not in monsoon:
+            ax.axvspan(j,j+4, facecolor = 'blue', alpha = 0.2)
 
     
-fig.text(0.04, 0.5, '[MCM]', ha='center', va='center', rotation='vertical', fontsize = 10)
+fig.text(0.04, 0.5, '[mm/month]', ha='center', va='center', rotation='vertical', fontsize = 10)
 plt.xlabel('Time [month]')
+plt.show()
+
+
+# Average precipitation
+fig, ax1 = plt.subplots(figsize=(15,5))
+ax1.plot(ntimes, prec_average, label = 'Precipitation')
+ax1.plot(ntimes, pet_average, label = 'PET')
+ax1.set_ylabel('[mm/month]')
+
+ax2 = ax1.twinx()
+ax2.plot(ntimes, grun_average, label = 'G-RUN', color = 'green')
+ax2.set_ylabel('[$m^3$/s]')
+for j in prec_filtered.index:
+    if j in monsoon and j-1 not in monsoon:
+        plt.axvspan(j,j+4, facecolor = 'blue', alpha = 0.2)
+ax1.legend(loc = 'upper right')
+ax2.legend(loc = 'upper right', bbox_to_anchor = (1.0, 0.80))
+
+plt.xlabel('time [Month]')
 plt.show()
 
 
